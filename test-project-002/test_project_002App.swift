fabe0fb1be346f8ef4b17d30df107caec61e698c
Basil Basaif <b.basaif@otekit.com>
2021-12-24 12:59:59 +0300

    Initial Commit

---
+++ test-project-002/test_project_002App.swift
@@ -0,0 +1,17 @@
+//
+//  test_project_002App.swift
+//  test-project-002
+//
+//  Created by Basil Basaif on 12/24/21.
+//
+
+import SwiftUI
+
+@main
+struct test_project_002App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
